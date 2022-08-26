e101888fbb109696045147909c6de268c11d24a6
Kazım Kağan İğci <61113947+KazimKaganIgci@users.noreply.github.com>
2022-08-26 20:02:23 +0300

    Update util.kt

--- ArtApp/app/src/main/java/com/kazim/artapp/util/util.kt
+++ ArtApp/app/src/main/java/com/kazim/artapp/util/util.kt
@@ -1,6 +1,6 @@
 package com.kazim.artapp.util
 
 object util {
-    const val API_KEY ="28820993-f654c5bac1c00fb9846304e06"
+    const val API_KEY ="Api key giriniz..."
     const val BASE_URL="https://pixabay.com"
 }
